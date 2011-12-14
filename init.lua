33a28809bc4b0774796f8273ace706b3cc5a1a97
Vladimir Dronnikov <dronnikov@gmail.com>
2011-12-14 13:21:20 +0400

    shortcuts operate on hex strings by default

--- init.lua
+++ init.lua
@@ -28,11 +28,11 @@ local function fromhex(str)
   end))
 end
 
-local function digest(method, salt, data, hex)
+local function digest(method, salt, data, raw)
   local digest = Crypto.get_digest(method, salt):init()
   digest:update(data)
   local result = digest:final()
-  if hex then result = tohex(result) end
+  if not raw then result = tohex(result) end
   return result
 end
 
@@ -40,28 +40,28 @@ end
 -- sugar
 --
 
-Crypto.md5 = function(data, hex)
-  return digest('md5', nil, data, hex)
+Crypto.md5 = function(data, raw)
+  return digest('md5', nil, data, raw)
 end
 
-Crypto.sha1 = function(data, hex)
-  return digest('sha1', nil, data, hex)
+Crypto.sha1 = function(data, raw)
+  return digest('sha1', nil, data, raw)
 end
 
 Crypto.sign = function(secret, data)
   return digest('sha1', secret, data, true)
 end
 
-Crypto.encrypt = function(secret, data, hex)
+Crypto.encrypt = function(secret, data, raw)
   local cipher = Crypto.get_cipher('aes192')
   local result = cipher:encrypt(data, secret)
-  if hex then result = tohex(result) end
+  if not raw then result = tohex(result) end
   return result
 end
 
-Crypto.uncrypt = function(secret, data, hex)
+Crypto.uncrypt = function(secret, data, raw)
   local cipher = Crypto.get_cipher('aes192')
-  if hex then data = fromhex(data) end
+  if not raw then data = fromhex(data) end
   return cipher:decrypt(data, secret)
 end
 
